d14ee0c421a9658872645dea5a710eccfd3c85d7
Francesco Saltori <francesco.saltori@it.clara.net>
2021-02-23 15:59:15 +0100

    Implement character healing

--- src/main/kotlin/com/xpeppers/kata/Character.kt
+++ src/main/kotlin/com/xpeppers/kata/Character.kt
@@ -5,10 +5,19 @@ import kotlin.math.min
 class Character {
     val isDead
         get() = health == 0
-    var health = 1000
+
+    var health = INITIAL_HEALTH
         private set
 
     fun dealDamageTo(target: Character, damage: Int) {
         target.health -= min(damage, target.health)
+    }
+
+    fun heal(target: Character, healingAmount: Int) {
+        target.health = min(target.health + healingAmount, INITIAL_HEALTH)
+    }
+
+    companion object {
+        private const val INITIAL_HEALTH = 1000
     }
 }
--- src/test/kotlin/com/xpeppers/kata/CharacterTest.kt
+++ src/test/kotlin/com/xpeppers/kata/CharacterTest.kt
@@ -4,10 +4,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CharacterTest {
+    private val character = Character()
+    private val target = Character()
+
     @Test
     fun `Characters can deal damage to Characters`() {
-        val character = Character()
-        val target = Character()
         val initialTargetHealth = target.health
 
         character.dealDamageTo(target, 100)
@@ -17,12 +18,28 @@ class CharacterTest {
 
     @Test
     fun `when damage received exceeds current Health, Health becomes 0 and the character dies`() {
-        val character = Character()
-        val target = Character()
-
         character.dealDamageTo(target, target.health + 1)
 
         assertThat(target.health).isEqualTo(0)
         assertThat(target.isDead).isTrue()
+    }
+
+    @Test
+    fun `a Character can heal a Character`() {
+        character.dealDamageTo(target, 100)
+        val targetHealthBefore = target.health
+
+        character.heal(target, 50)
+
+        assertThat(target.health).isEqualTo(targetHealthBefore + 50)
+    }
+
+    @Test
+    fun `healing cannot raise health above initial health`() {
+        val initialTargetHealth = target.health
+
+        character.heal(target, 50)
+
+        assertThat(target.health).isEqualTo(initialTargetHealth)
     }
 }
